4e7a0668813bf83d3dc2e3df7eb3b02a55f90158
Alan-2023Dts <150711078+Alan-2023Dts@users.noreply.github.com>
2025-10-26 03:55:30 -0600

    Resolución de bugs en menu.py
    Errores de escritura y de logica

--- menu.py
+++ menu.py
@@ -3,8 +3,8 @@ Módulo del menú principal
 Maneja toda la interfaz de usuario de la máquina expendedora
 """
 
+
 from maquina_expendedora import MaquinaExpendedora
-from utilidades import *
 
 def mostrar_menu_principal():
 #Funcion que unicamente muestra un menu al usuario
@@ -23,7 +23,6 @@ def procesar_opcion_menu(opcion, maquina):
     
     # Opción 1: Mostrar productos disponibles
     # TODO: Llamar al método correspondiente de la máquina
-    
     if opcion == 1:
         maquina.mostrar_productos_disponibles() #El orden para llamar la funcion correspondiente es primero la clase maquina y luego el nombre de la funcion
  
@@ -47,13 +46,12 @@ def procesar_opcion_menu(opcion, maquina):
     elif opcion == 3:
         codigo = input("Ingresa el codigo del producto que deseas: ")
         if validar_codigo_producto(codigo):# llamamos a la funcion de utilidades que valida que el codigo tenga una letra y un numero en el orde 1A (la funcion devuelve un true/false)
-            maquina.selecionar_producto(codigo) 
+            maquina.seleccionar_producto(codigo) 
         else:
             print("El codigo ingresado es invalido ")
     
     # Opción 4: Ver dinero insertado
     # TODO: Llamar al método correspondiente de la máquina
-    
     elif opcion == 4:
         maquina.mostrar_dinero_insertado() #Unicamente muestra cuanto dinero ya se inserto(es para recordar al usuario que cantidad tiene)
         
@@ -72,7 +70,7 @@ def procesar_opcion_menu(opcion, maquina):
         return False #este false apaga el bucle y sale del programa
     else:
         print("Opcion invalida")
-    pass
+    return True  # Agregar esta línea
 
 #           Codigo Descartado
 ############################################################################################
@@ -103,47 +101,5 @@ def procesar_opcion_administrador(opcion, maquina):
     pass
 ###############################################################################################
 
-def ejecutar_maquina_expendedora():
-    """
-    Función principal que ejecuta la máquina expendedora
-    """
-    # TODO: Crear una instancia de MaquinaExpendedora
-    # TODO: Cargar los productos iniciales
-    # TODO: Mostrar el título
-    # TODO: Crear un bucle principal que:
-    #       - Limpie la pantalla
-    #       - Muestre el menú principal
-    #       - Pida al usuario que seleccione una opción
-    #       - Procese la opción
-    #       - Pause antes de continuar (excepto al salir)
-    # TODO: El bucle debe continuar hasta que el usuario elija salir
-    # 
-    # Ejemplo de implementación:
-    # maquina = MaquinaExpendedora()
-    # maquina.cargar_productos_iniciales()
-    # 
-    # mostrar_titulo()
-    # 
-    # continuar = True
-    # while continuar:
-    #     mostrar_menu_principal()
-    #     opcion = input("Selecciona una opción: ")
-    #     
-    #     continuar = procesar_opcion_menu(opcion, maquina)
-    #     
-    #     if continuar:
-    #         pausar()
-    #         limpiar_pantalla()
-    # 
-    # El bucle while se ejecuta hasta que continuar sea False
-    # Esto pasa cuando el usuario elige la opción 7 (salir)
-    pass
 
-if __name__ == "__main__":
-    # TODO: Llamar a la función ejecutar_maquina_expendedora()
-    # 
-    # Ejemplo de implementación:
-    # ejecutar_maquina_expendedora()
-    # 
-    # Esta línea hace que el programa se ejecute cuando corras este archivo
-    pass
+
